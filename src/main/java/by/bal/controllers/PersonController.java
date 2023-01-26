296d8f1063ad61444dd81ac48e9de778c404b782
Pavel Bal <pbal00@mail.ru>
2023-01-26 15:17:41 +0300

    feat: main commit

---
+++ src/main/java/by/bal/controllers/PersonController.java
@@ -0,0 +1,50 @@
+package by.bal.controllers;
+
+import by.bal.dao.PersonDAO;
+import by.bal.models.Person;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PersonController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "new";
+
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+}
